18cdd56a9ff43ed14b6b9d7ce4d537b3d9866dd5
Nicholas Seymour <160050029+SeymourPrograms@users.noreply.github.com>
2024-02-14 16:24:50 -0500

    Add files via upload

---
+++ firstscriptonmac.py
@@ -0,0 +1,11 @@
+#This is to test out the coding on macbok linux vscode and 
+#also try out github for the very first time
+
+print("Welcome to Seymour's first crack at Python in a fat minute, \nand also his first attempt at github.\nNeato!!")
+
+response1 = input("Let's make this a little more interactive.\nKnock Knock\n")
+
+if response1 == "Who's there?" or response1 == "who is it" or response1 == "whos there" or response1 == "who is it?":
+    print("Orange ya glad I didn't say banana?\nSorry, I am a machine and not very good at jokes.")
+else:
+    print("You think this shit is funny? Answer a knock knock question correctly or I will use my AI powers to cripple the world economy")
